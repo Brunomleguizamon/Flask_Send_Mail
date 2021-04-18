3faa727b2f8f2f89cb5807853b0ff96cb357f56e
Bmleguizamon <brunomleguizamon@gmail.com>
2021-04-18 19:07:59 -0300

    make it better

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+PORT = 5000
+DEBUG = False
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return "Not Found."
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello World from app flask mail"
+
+
+if __name__ == '__main__':
+    app.run(port=PORT, debug=DEBUG)
